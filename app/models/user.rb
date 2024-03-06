f2468846928f4101049df418d495d736f8ccefe4
“YuriShozen” <155799910+w2yuri@users.noreply.github.com>
2024-03-06 13:11:25 +0000

    iine

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :favorites, dependent: :destroy
   has_many :books
   has_one_attached :profile_image
 
