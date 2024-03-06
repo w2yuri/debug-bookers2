bca69584d8f2064b86f7404cea51221bc1f5ff64
“YuriShozen” <155799910+w2yuri@users.noreply.github.com>
2024-03-06 14:52:50 +0000

    Finish フォローフォロワー機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,12 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :books
   has_one_attached :profile_image
+  
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followings, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction,    length: { maximum: 50 } 
@@ -16,4 +22,19 @@ class User < ApplicationRecord
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
+  
+  def follow(user)
+    active_relationships.create(followed_id: user.id)
+  end 
+  
+  def unfollow(user)
+      relationship = active_relationships.find_by(followed_id: user.id)
+      relationship.destroy if relationship
+  end
+  
+  
+  def following?(user)
+    followings.include?(user)
+  end 
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,11 @@ Rails.application.routes.draw do
   resources :books, only: [:index, :show, :edit, :create, :destroy, :update] do
    resource :favorites, only: [:create, :destroy]
   end
-  resources :users, only: [:index,:show,:edit,:update]
+  resources :users, only: [:index,:show,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+       get "followings" => "relationships#followings", as: "followings"
+       get "followers" => "relationships#followers", as: "followers"
+  end
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end 
